fb1fb20cbb36b9f3f213bd0522f7f37edae63b5b
emon4075 <mdabdullahemon4075@gmail.com>
2024-03-14 08:44:57 +0600

    Pointer Problem Solved

--- Exam_Preparation/Pointer_2.c
+++ Exam_Preparation/Pointer_2.c
@@ -1,10 +1,12 @@
+
 #include <stdio.h>
-int main()
-{
-  int x=10, *ptr=&x, y=*ptr;
-  printf(“%d ia at= %d\n”, *&x, &x);
-  printf(“%d ia at= %d\n”, *ptr, ptr);
-  printf(“%d ia at= %d\n”, y, &*ptr);
-  printf(“%d ia at= %d\n”, ptr, &ptr);
+
+int main() {
+  int x = 10, *ptr = &x, y = *ptr;
+  printf("%d is at= %p\n", *(&x), (void *)&x);
+  printf("%d is at= %p\n", *ptr, (void *)ptr);
+  printf("%d is at= %p\n", y, (void *)&(*ptr));
+  printf("%p is at= %p\n", (void *)ptr, (void *)&ptr);
   return 0;
 }
+
---
+++ Exam_Preparation/main.c
@@ -0,0 +1,8 @@
+// Try Out The Bat Command
+#include<stdio.h>
+int main()
+{
+  int i=3,j=4,k=5;
+  printf("%d",i%j + i<k);
+  return 0;
+}
